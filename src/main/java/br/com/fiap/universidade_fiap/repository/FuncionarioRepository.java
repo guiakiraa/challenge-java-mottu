294a05be9be89e8e924d4c0464a3cb83cc7bdc00
guiakiraa <guilhermeakira0810@gmail.com>
2025-10-01 16:12:08 -0300

    fluxos sem ser o crud

--- src/main/java/br/com/fiap/universidade_fiap/repository/FuncionarioRepository.java
+++ src/main/java/br/com/fiap/universidade_fiap/repository/FuncionarioRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.fiap.universidade_fiap.model.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+    long countByFilial_Id(Long filialId);
 }
 
 
